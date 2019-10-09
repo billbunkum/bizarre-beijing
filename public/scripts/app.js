9706f4a0dff46d65c188159e46bafadb83bde48e
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-10-09 14:46:26 -0400

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('here');
+
+var template = React.createElement('div', null);
+var appRoot = document.getElementById('app');
+
+// ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log('here');
+
+const template = (
+	<div>
+    {/* <div class="main-content">
+      <header>
+        <h1>Bizarre Beijing</h1>
+        <a href="/">Home</a>
+        <a href="/news.html">News</a>
+        <a href="/contact_us.html">Contact Us</a>
+      </header>
+        Coming Soon...
+    </div>
+
+    <footer>© Bizarre Beijing 2019</footer> */}
+  </div>
+);
+const appRoot = document.getElementById('app');
+
+// ReactDOM.render(template, appRoot);
