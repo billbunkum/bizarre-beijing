1fb774b787664bb76828d559277537f15c246ce2
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-10-17 17:26:46 -0400

    begin implementing dynamic over static

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
-// THIS MAY EVENTUALLY BE USED TO POPULATE
-// UpcomingEvents.js
+// PLAYGROUND
 
 const Events = () => {
-  return (
-    <div>
+  const eventDetails = {
+    capacity: 18,
+    date: "October 18, 2019",
+    endTime: "9:00pm",
+    imagePath: "/img/xuanwumen_church.png",
+    price: "¥280",
+    place: "Xuanwumen (in front of Xuanwumen Catholic Church)",
+    startTime: "6:30pm",
+    title: "October 18th Haunt Tour"
+  };
 
+  return (
+    <div className="upcoming-event">
+      <div className="upcoming-event__header">
+        <h3 className="upcoming-event__title">{eventDetails.title}</h3>
+        {this.props.soldOut && <p><Messages /></p>}
+        <img src={eventDetails.imagePath}
+          className="upcoming-event__image"
+          />
+      </div>
+      <div className="upcoming-event__abstract">
+        <p className="upcoming-event__abstract__directions">
+          Date: {eventDetails.date}<br />
+          Time: {eventDetails.startTime} - {eventDetails.endTime}<br />
+          Price: {eventDetails.price}<br />
+          Place: {eventDetails.place} 
+        </p>
+        <p className="upcoming-event__abstract__details">
+          We will gather at 6:15 and depart at 6:30 sharp.<br />
+          Due to safety concerns in regards to possible attacks by supernatural forces, we will limit the number of participants to <em>{eventDetails.capacity} people</em>.
+        </p>
+      </div>
     </div>
   )
 };
--- src/components/UpcomingEvent.js
+++ src/components/UpcomingEvent.js
@@ -5,41 +5,63 @@ import MoreInfoButton from './MoreInfoButton.js';
 import RSVPButton from './RSVPButton.js';
 
 class UpcomingEvent extends React.Component { 
-
-// MAY NEED LOGIC TO DISPLAY events array FROM Events COMPONENT
-
   render() {
+    const event = [
+      {
+        capacity: 18,
+        date: "October 18, 2019",
+        endTime: "9:00pm",
+        imagePath: "/img/xuanwumen_church.png",
+        price: "¥280",
+        place: "Xuanwumen (in front of Xuanwumen Catholic Church)",
+        soldOut: true,
+        startTime: "6:30pm",
+        title: "October 18th Haunt Tour"
+      },
+      {
+        capacity: 18,
+        date: "October 26, 2019",
+        endTime: "9:00pm",
+        imagePath: "/img/xuanwumen_church2.jpg",
+        price: "¥280",
+        place: "Xuanwumen (in front of Xuanwumen Catholic Church)",
+        soldOut: true,
+        startTime: "6:30pm",
+        title: "October 26th Haunt Tour"
+      }
+    ];
     return (
       <div className="main-content">
         <h2>Upcoming Events</h2>
+
+{/* START GENERATING EVENTS HERE */}
+
         <div className="upcoming-event">
           <div className="upcoming-event__header">
-            <h3 className="upcoming-event__title">Friday 13th Haunt Tour</h3>
+            <h3 className="upcoming-event__title">{event[0].title}</h3>
             {this.props.soldOut && <p><Messages /></p>}
-            <img src="/img/xuanwumen_church.png"
+            <img src={event[0].imagePath}
               className="upcoming-event__image"
               />
           </div>
           <div className="upcoming-event__abstract">
             <p className="upcoming-event__abstract__directions">
-              Date: September 13, 2019<br />
-              Time: 6:00 – 9:00pm<br />
-              Price: ¥280<br />
-              Place: Xuanwumen (in front of Xuanwumen Catholic Church).
+              Date: {event[0].date}<br />
+              Time: {event[0].startTime} - {event[0].endTime}<br />
+              Price: {event[0].price}<br />
+              Place: {event[0].place} 
             </p>
             <p className="upcoming-event__abstract__details">
-              We will gather at 6:00 and depart at 6:15 sharp.<br />
-              Due to safety concerns in regards to possible attacks by supernatural forces, we will limit the number of participants to <em>18 people</em>.
+              We will gather at 6:15 and depart at 6:30 sharp.<br />
+              Due to safety concerns in regards to possible attacks by supernatural forces, we will limit the number of participants to <em>{event[0].capacity} people</em>.
             </p>
-          
           </div>
           <div className="upcoming-event__buttons">
               <RSVPButton />
               <MoreInfoButton />
           </div>
-        
         </div>
-        
+
       </div>
     )
   };
